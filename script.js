d76b450e9396879704fb218789d65500b68021fa
Long <long.nguyen@ntq-solution.com.vn>
2018-05-16 17:33:43 +0700

    load data

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var chayNgayDi = document.getElementById("chay-ngay-di");
 
 width = 0;
 flag = 1;
-setInterval(() => {
+
+var cc = setInterval(() => {
 	chayNgayDi.style.marginLeft = width + 'px';
 	chayNgayDi.style.background = 'green';
 	if (flag === 1) {
@@ -15,4 +16,32 @@ setInterval(() => {
 	} else if(width === 0) {
 		flag = 1;
 	}
-}, 1000);
+}, 16);
+
+function showHang() {
+	startLayHang = Date.now()
+	console.log('before', width);
+	// console.log(startLayHang);
+
+	setTimeout(() => {
+		var xhttp = new XMLHttpRequest();
+		number = 0;
+	  	xhttp.onreadystatechange = function() {
+		    if (this.readyState == 4 && this.status == 200) {
+		    	setInterval(() => {
+		    		number ++;
+		    		if (number < 10000) {
+		    			document.getElementById("show-hang").innerHTML += '<p>this.responseText</p>';
+		    		}
+		    	}, 15)
+		    	
+		    	
+		    	finishLayHang = Date.now();
+		    	console.log((finishLayHang - startLayHang));
+		    	console.log('after', width);
+		    }
+		};
+		xhttp.open("GET", "data.txt", true);
+		xhttp.send();
+	}, 0);
+}
